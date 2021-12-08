1ff1895f7d222e42d65d8d4f28c2d39754f152bf
Diogo Kuiaski <diogokuiaski@users.noreply.github.com>
2021-12-08 12:03:37 -0300

    [#3] Adding solutions to day2
    Only recursive solutions. Try next time just run over the array.
    
    Adding some more test cases due not cleaning the hashmap.

--- day.02/min_cost_climbing_stairs_test.go
+++ day.02/min_cost_climbing_stairs_test.go
@@ -10,13 +10,14 @@ type minCostStairsTest struct {
 var minCostStairsTests = []minCostStairsTest{
 	{[]int{10, 15, 20}, 15},
 	{[]int{1, 100, 1, 1, 1, 100, 1, 1, 100, 1}, 6},
+	{[]int{0, 0, 1, 1}, 1},
 }
 
 func TestMinCostClimbingStairs(t *testing.T) {
 	t.Log("[DynamicPrograming Day 2 Testing MinCostClimbingStairs")
 	for _, test := range minCostStairsTests {
 		if output := minCostClimbingStairs(test.n); output != test.expected {
-			t.Errorf("Input %d :: output %d not equal to expected %d", test.n, output, test.expected)
+			t.Errorf("Input %v :: output %d not equal to expected %d", test.n, output, test.expected)
 		}
 	}
 }
