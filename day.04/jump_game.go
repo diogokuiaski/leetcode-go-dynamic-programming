a570d4b8c3fc6f7f1dd8a852ef8812248ad6bdb5
Diogo Kuiaski <diogokuiaski@users.noreply.github.com>
2021-12-14 11:07:23 -0300

    [#16] Add day 4 problems
    Adding problems and some unit tests

---
+++ day.04/jump_game.go
@@ -0,0 +1,5 @@
+package day04
+
+func canJump(nums []int) bool {
+	return false
+}
---
+++ day.04/jump_game_2_test.go
@@ -0,0 +1,22 @@
+package day04
+
+import "testing"
+
+type jumpGame2Test struct {
+	n        []int
+	expected int
+}
+
+var jumpGame2Tests = []jumpGame2Test{
+	{[]int{2, 3, 1, 1, 4}, 2},
+	{[]int{3, 2, 1, 0, 4}, 2},
+}
+
+func TestJumpGame2(t *testing.T) {
+	t.Log("[DynamicPrograming Day 3 Testing JumpGame2")
+	for _, test := range jumpGame2Tests {
+		if output := jump(test.n); output != test.expected {
+			t.Errorf("Input %v :: output %d not equal to expected %d", test.n, output, test.expected)
+		}
+	}
+}
