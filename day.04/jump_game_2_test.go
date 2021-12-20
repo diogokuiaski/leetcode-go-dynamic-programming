2b32c04e3e6c48c34d63c38142bfa8c8d75f39b1
Diogo Kuiaski <diogokuiaski@users.noreply.github.com>
2021-12-20 17:39:07 -0300

    Add solution to day 04 Fixes #18

--- day.04/jump_game_2_test.go
+++ day.04/jump_game_2_test.go
@@ -9,13 +9,16 @@ type jumpGame2Test struct {
 
 var jumpGame2Tests = []jumpGame2Test{
 	{[]int{2, 3, 1, 1, 4}, 2},
-	{[]int{3, 2, 1, 0, 4}, 2},
+	{[]int{2, 3, 1, 0, 4}, 2},
 }
 
 func TestJumpGame2(t *testing.T) {
-	t.Log("[DynamicPrograming Day 3 Testing JumpGame2")
+	t.Log("[DynamicPrograming Day 4 Testing JumpGame2")
 	for _, test := range jumpGame2Tests {
 		if output := jump(test.n); output != test.expected {
+			t.Errorf("Input %v :: output %d not equal to expected %d", test.n, output, test.expected)
+		}
+		if output := jump2Best(test.n); output != test.expected {
 			t.Errorf("Input %v :: output %d not equal to expected %d", test.n, output, test.expected)
 		}
 	}
--- day.04/jump_game_test.go
+++ day.04/jump_game_test.go
@@ -10,12 +10,20 @@ type jumpGameTest struct {
 var jumpGameTests = []jumpGameTest{
 	{[]int{2, 3, 1, 1, 4}, true},
 	{[]int{3, 2, 1, 0, 4}, false},
+	{[]int{1}, true},
+	{[]int{0}, true},
+	{[]int{0, 1}, false},
+	{[]int{2, 0, 1, 1, 4}, true},
+	{[]int{1, 0, 1, 1, 4}, false},
 }
 
 func TestJumpGame(t *testing.T) {
 	t.Log("[DynamicPrograming Day 3 Testing JumpGame")
 	for _, test := range jumpGameTests {
 		if output := canJump(test.n); output != test.expected {
+			t.Errorf("Input %v :: output %t not equal to expected %t", test.n, output, test.expected)
+		}
+		if output := bestCanJump(test.n); output != test.expected {
 			t.Errorf("Input %v :: output %t not equal to expected %t", test.n, output, test.expected)
 		}
 	}
